34c6ffee6b0b9dd6c40281ea0b17aaaf07ca6d6a
joaovnpt <joaovitorguiotti8@gmail.com>
2024-07-17 11:28:26 -0300

    definido equipe

---
+++ src/views/EquipeView.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    name: 'EquipeView',
+    data() {
+        return {
+            equipe: [
+                {
+                    nome: 'João Vitor Guiotti',
+                    cargo: 'Desenvolvedor'
+                },
+                {
+                    nome: 'Gabriel Campregher Mohr',
+                    cargo: 'Designer'
+                },
+                {
+                    nome: 'Luiz Miguel Lima de Souza',
+                    cargo: 'Gerente de Projetos'
+                },
+                {
+                    nome: 'Brayan Pedroso de Mattos',
+                    cargo: 'desenvolvedor'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>Equipe</h1>
+        <ul>
+            <li v-for="membro in equipe" :key="membro.nome">
+                <h2>{{ membro.nome }}</h2>
+                <p>{{ membro.cargo }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+/* Your component-specific styles go here */
+</style>
